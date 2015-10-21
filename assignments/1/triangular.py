fda0471da1212a0c7be6cf5b750bfcc84380e6ff
John Goiri <jg.goiri@gmail.com>
2015-10-21 12:42:06 -0700

    Made printing prettier

--- assignments/1/triangular.py
+++ assignments/1/triangular.py
@@ -9,6 +9,17 @@ b=np.matrix([[-0.5],[math.sqrt(3)/2]])
 triangluar=sm2d.Crystal(a,b,[])
 pgroup=sorted(triangluar.point_group())
 
+print "The lattice is defined as"
+print triangluar.lattice
+print ""
+
+print "The point group operations for this lattice are:"
+for op in pgroup:
+    print op.name
+    print op.matrix
+    print ""
+
+print "The multiplication table of the point_group is:"
 tablestring=""
 for op1 in pgroup:
     for op2 in pgroup:
@@ -18,7 +29,7 @@ for op1 in pgroup:
         tau=np.matrix([[0],[0]])
         prodmat=mat1.dot(mat2)
         prodsymop=sm2d.symmetry.SymOp(prodmat,tau)
-        tablestring+=prodsymop.name+"    "
+        tablestring+=prodsymop.name+" "*(8-len(prodsymop.name))
     tablestring+="\n"
 
 print tablestring
