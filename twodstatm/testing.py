18b1c3601d17a9b5b25d884ba26a3ffb8b3e7d7e
John Goiri <jg.goiri@gmail.com>
2015-10-24 20:00:17 -0700

    Added useless testing file

---
+++ twodstatm/testing.py
@@ -0,0 +1,10 @@
+import twodstatm as sm2d
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+#define triangular lattice
+a=np.matrix([[1],[0]])
+b=np.matrix([[-0.5],[math.sqrt(3)/2]])
+
+lat=np.hstack((a,b))
