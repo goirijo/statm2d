fdf9d3cdb8ffbb88777b51da32bc3ee5ddeecf91
John Goiri <jg.goiri@gmail.com>
2015-11-04 18:35:13 -0800

    Starting assignment 3...

---
+++ assignments/3/dispersion.py
@@ -0,0 +1,98 @@
+import statm2d as sm2d
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+#define triangular lattice
+a=np.matrix([[1],[0]])
+b=np.matrix([[-0.5],[math.sqrt(3)/2]])
+coord1=np.matrix([0,0]).T
+coord2=(2*a+b)/3
+site1=sm2d.structure.Site(coord1,"A")
+site2=sm2d.structure.Site(coord2,"A")
+
+triangluar=sm2d.Crystal(a,b,[site1],fracmode=False)
+honeycomb=sm2d.Crystal(a,b,[site1,site2],fracmode=False)
+pgroup=triangluar.point_group()
+
+pair0=sm2d.structure.Site(np.matrix([[0],[0]]),"A",triangluar._lattice,True)
+pair1=sm2d.structure.Site(np.matrix([[1],[0]]),"A",triangluar._lattice,True)
+#pair1=site2
+
+print "The selected pair in the triangluar lattice is"
+print pair0
+print pair1
+print ""
+
+fig=plt.figure()
+ax=fig.add_subplot('111')
+ax.set_title(r"\bf{Triangular lattice pair cluster}")
+ax.set_xlabel(r"\bf{x}")
+ax.set_ylabel(r"\bf{y}")
+triangluar.plot(ax,(-1,4),(-1,4))
+ax.scatter(pair0._coord[0],pair0._coord[1],color="r",s=70)
+ax.scatter(pair1._coord[0],pair1._coord[1],color="r",s=70)
+plt.show()
+
+submap,subswitch=sm2d.phonon.cluster_subgroups(pair0,pair1,pgroup)
+print "The following operations mapped the cluster sites onto themselves:"
+for op in submap:
+    print op
+    #print op.tex_formula()
+    print ""
+print "The following operations mapped the cluster sites onto each other:"
+for op in subswitch:
+    print op
+    print ""
+
+tensorbasis=sm2d.phonon.force_tensor_basis_for_pair(pair0,pair1,pgroup)
+
+print "The invariant tensor basis is:"
+for basis in tensorbasis:
+    print basis
+    print ""
+    #print "\\begin{equation}"
+    #print "    \\Lambda="
+    #print sm2d.misc.matrix_tex(basis,1)
+    #print "    \\label{symbasis}"
+    #print "\\end{equation}"
+    #print ""
+
+print "The linearly independent basis are "+str(sm2d.misc.independent_indices(tensorbasis))
+
+pair0=site1
+pair1=site2
+
+print "The selected pair in the honeycomb structure is"
+print pair0
+print pair1
+print ""
+
+fig=plt.figure()
+ax=fig.add_subplot('111')
+ax.set_title(r"\bf{Honeycomb pair cluster}")
+ax.set_xlabel(r"\bf{x}")
+ax.set_ylabel(r"\bf{y}")
+honeycomb.plot(ax,(-1,4),(-1,4))
+ax.scatter(pair0._coord[0],pair0._coord[1],color="r",s=70)
+ax.scatter(pair1._coord[0],pair1._coord[1],color="r",s=70)
+plt.show()
+
+submap,subswitch=sm2d.phonon.cluster_subgroups(pair0,pair1,pgroup)
+print "The following operations mapped the cluster sites onto themselves:"
+for op in submap:
+    print op
+    print ""
+print "The following operations mapped the cluster sites onto each other:"
+for op in subswitch:
+    print op
+    print ""
+
+tensorbasis=sm2d.phonon.force_tensor_basis_for_pair(pair0,pair1,pgroup)
+
+print "The invariant tensor basis is:"
+for basis in tensorbasis:
+    print basis
+    print ""
+
+print "The linearly independent basis are "+str(sm2d.misc.independent_indices(tensorbasis))
--- statm2d/phonon.py
+++ statm2d/phonon.py
@@ -3,9 +3,38 @@ import numpy as np
 import symmetry as sym
 import structure
 
+def map_cluster(site0,site1,op):
+    """Checks if the symmetry operation
+    maps a cluster onto itself (with a translation).
+    If it does, check if the sites switched places.
+
+    :site0: Site
+    :site1: Site
+    :op: Op
+    :returns: Site,Site,bool,bool
+
+    """
+    transsite0=site0.apply_symmetry(op)
+    transsite1=site1.apply_symmetry(op)
+    transdelta,transmap=structure.site_delta(transsite0,transsite1)
+    clusterdelta,basismap=structure.site_delta(site0,site1)
+
+    goodmap=False
+    mapswitch=False
+    
+    if np.allclose(clusterdelta,transdelta):
+        goodmap=True
+
+    #if the deltas have opposite signs, the sites map by translation, but they switched places
+    elif np.allclose(-clusterdelta,transdelta) and basismap:
+        goodmap=True
+        mapswitch=True
+
+    return transsite0,transsite1,goodmap,mapswitch
+
 def cluster_subgroups(site0,site1,symgroup):
     """Determine the symmetry operations of the given
-    symgroup that leave the cluster unchanged (with
+    symgroup that leave the pair cluster unchanged (with
     a translation). Check if the translation to map
     back involves sites switching places.
     The first list of operations mapped the sites
@@ -14,24 +43,23 @@ def cluster_subgroups(site0,site1,symgroup):
 
     :site0: Site
     :site1: Site
+    :symgroup: list of Op
     :returns: two lists of Op
 
     """
     subgroupmap=[]
     subgroupswitch=[]
-    clusterdelta,basismap=structure.site_delta(site0,site1)
+    
     for op in symgroup:
-        transsite0=site0.apply_symmetry(op)
-        transsite1=site1.apply_symmetry(op)
-        transdelta,transmap=structure.site_delta(transsite0,transsite1)
+        transsite0,transsite1,goodmap,mapswitch=map_cluster(site0,site1,op)
 
         #if the deltas remain the same, then the sites map onto themselves by translation
-        if np.allclose(clusterdelta,transdelta):
+        if goodmap and not mapswitch:
             op.shift=site0._coord-transsite0._coord
             subgroupmap.append(op)
 
         #if the deltas have opposite signs, the sites map by translation, but they switched places
-        elif np.allclose(-clusterdelta,transdelta) and basismap:
+        elif goodmap and mapswitch:
             op.shift=site1._coord-transsite0._coord
             subgroupswitch.append(op)
 
@@ -62,3 +90,25 @@ def force_tensor_basis_for_pair(site0,site1,symgroup):
         extrasymmetrized=sym.matrix_reynolds(candidate.T,switchgroup)
         basiscandidates[idx]=(symmetrized+extrasymmetrized)/(len(mapgroup)+len(switchgroup))
     return basiscandidates
+
+def equivalent_clusters(site0,site1,symgroup):
+    """Apply the given group of symmetry operations onto
+    the given pair cluster, and find new symmetrically
+    equivalent clusters. Make list of resulting clusters
+    and list of operations that resulted in a new cluster.
+
+    :site0: Site
+    :site1: Site
+    :symgroup: list of Op
+    :returns: list of Site,Site,Op
+
+    """
+    equivsite0=[]
+    equivsite1=[]
+    mapsym=[]
+
+    for op in symgroup:
+        transsite0=site0.apply_symmetry(op)
+        transsite1=site1.apply_symmetry(op)
+        transdelta,transmap=structure.site_delta(transsite0,transsite1)
+
